176ee1595dca3a0960724ffee2663282b12e32f2
Siggi Skulason <61322661+siggiskulason@users.noreply.github.com>
2020-09-04 16:01:20 +0100

    Update run-checkbox.sh

--- tests/run-checkbox.sh
+++ tests/run-checkbox.sh
@@ -1,7 +1,7 @@
+ls ~/tests/
 mkdir ~/checkbox
 cd ~/checkbox
 git clone https://git.launchpad.net/~checkbox-dev/checkbox-provider-edgex/+git/checkbox-provider-edgex
-ls
 
 # override the utils.sh file so that we install a local snap
 cp ~/tests/utils.sh checkbox-provider-edgex/data/geneva
